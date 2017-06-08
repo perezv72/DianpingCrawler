a09c52e1a60e77343a262d0c8ed3c63173e80027
Victor Perez <perez.victor72@gmail.com>
2017-06-08 10:12:42 -0500

    Minor fixes
    Fixed some typos in the notes.

--- fetch.py
+++ fetch.py
@@ -16,18 +16,18 @@ chrome_options.add_experimental_option( "prefs",{'profile.managed_default_conten
 driver = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
 wait = WebDriverWait(driver, 10)
 
-# The file containing the links should be in the working directory or make sure that the command has teh correct path.
+# The file containing the links should be in the working directory or make sure that the command has the correct path.
 with open("urls.txt") as f:
     urls = f.readlines()
 
-# Checks if director title html exists, if not, it creates directory.
+# Checks if directory name "html" exists, if not, it creates directory.
 newpath = r'html'
 if not os.path.exists(newpath):
     os.makedirs(newpath)
 
 driver.get("http://www.dianping.com/search/keyword/58/0_%E6%B1%89%E5%A0%A1")
 Print("Diangping recently changed the architecture of their website. The old web site looks like Yelp while the new website looks more like Google Maps. Please review website before continuing")
-conti = input("Press 1 if page is new format, 2 if page is old style, 0 for to quit ")
+conti = input("Press 1 if page is new format (Looks like Google Maps), 2 if page is old style (Looks like Yelp), 0 to quit ")
 
 if conti==0:
     driver.quit()
@@ -37,6 +37,7 @@ elif conti == 2:
 elif conti == 1:
     snext = "NextPage"
 
+# function checks if there is more than one page with results
 def checkpages(url):
     try:
         driver.find_element_by_class_name(snext)
@@ -44,6 +45,7 @@ def checkpages(url):
     except NoSuchElementException:
         return False
 
+# function saves every html page and names it by numerical order
 def htmlwriter():
     n = len([name for name in os.listdir('.') if os.path.isfile(name)]) 
     n1 = "%04d"%n
